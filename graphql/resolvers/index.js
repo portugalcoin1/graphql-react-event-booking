d79edc032b293fa938c1751955168972f3de862a
Miguel Rocha <miguelrocha@lvengine.com>
2019-11-14 17:53:28 +0000

    Booking
    CreateBooking

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcryptjs'); // Pack para fazer a encriptação das passw
 const Event = require('../../models/event');
 // Importação do modelo do User
 const User = require('../../models/user');
+const User = require('../../models/boking');
 
 // buscar(fetch) multiplos ID
 // eventIds é o argumento
@@ -100,19 +101,23 @@ module.exports = {
         // verificar se já existe algum user com o email
         // a função findOne faz o filto pelo argumento email para comparar na BD dos users
         try{
-        const existingUser = await User.findOne({email: args.userInput.email})
-            if (existingUser) {
-                throw new Error('User exists already.');
-            }
+            const existingUser = await User.findOne({email: args.userInput.email})
+                if (existingUser) {
+                    throw new Error('User exists already.');
+                }
             const hashedPassword = await bcrypt.hash(args.userInput.password, 12); // faz a encriptação da pass e o nível é o 12 (safe)
-        // retornar dados do utilizar (incluindo a pwd encriptada) quando criado o user para a BD    
+            // retornar dados do utilizar (incluindo a pwd encriptada) quando criado o user para a BD    
             const user = new User({
                 email: args.userInput.email,
                 password: hashedPassword
             });
             const result = await user.save();
-        
-            return { ...result._doc, password: null,  _id: result._doc_id };
+            
+            return { 
+                ...result._doc, 
+                password: null,  
+                _id: result._doc_id 
+            };
         } catch (err) {
             throw err;
         }
