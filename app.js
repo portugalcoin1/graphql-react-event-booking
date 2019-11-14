dd708d08788466ef490f91b7f09bcff11ea4a66f
Miguel Rocha <miguelrocha@lvengine.com>
2019-11-14 14:15:05 +0000

    Convert to Await
    Convert functions to async and await

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHTTP = require('express-graphql'); // Pack do express-graphql
-const { buildSchema } = require('graphql'); // Schema do GraphQL
 const mongoose = require('mongoose'); // Pack para ligação com o mongoDB
-const bcrypt = require('bcryptjs'); // Pack para fazer a encriptação das passwords
 
-// Importação do modelo do Event
-const Event = require('./models/event');
-// Importação do modelo do User
-const User = require('./models/user');
+const graphQlSchema = require('./graphql/schema/index'); // chamada ao ficheiro do index do schema
+const graphQlResolvers = require('./graphql/resolvers/index'); // chamada ao ficheiro do index do schema
 
 const app = express();
 
@@ -17,35 +13,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// buscar(fetch) multiplos ID
-// eventIds é o argumento
-const events = eventIds => {
-    // $in operador especial do MongoDB
-    return Event.find({ _id: { $in: eventIds } })
-    .then(events => {
-        return events.map(event => {
-            return { 
-                ...event._doc, 
-                _id: event.id, 
-                creator: user.bind(this, event.creator) 
-            };
-        });
-    })
-    .catch(err => {
-        throw err;
-    })
-}
-
-const user = userId => {
-    return User.findById(userId)
-        .then( user => {
-            return { ...user._doc, _id: user.id, createdEvents: events.bind(this, user._doc.createdEvents) };
-        })
-        .catch(err => {
-            throw err;
-        });
-};
-
 /*app.get('/', (re, res, next) => {
     res.send('hello world!');
 })*/
@@ -53,136 +20,8 @@ const user = userId => {
 app.use(
     '/graphql', 
     graphqlHTTP({
-        schema: buildSchema(`
-            type Event {
-                _id: ID!
-                title: String
-                description: String!
-                price: Float!
-                date: String!
-                creator: User!
-            }
-
-            type User {
-                _id: ID!
-                email: String!
-                password: String
-                createdEvents:[Event!]
-            }
-
-            input EventInput {
-                title: String
-                description: String
-                price: Float
-                date: String
-            }
-
-            input UserInput {
-                email: String!
-                password: String
-            }
-
-            type RootQuery {
-                events: [Event!]!
-            }
-
-            type RootMutation {
-                createEvent(eventInput: EventInput): Event
-                createUser(userInput: UserInput): User
-            }
-
-            schema {
-                query: RootQuery
-                mutation: RootMutation
-            }
-        `),
-        rootValue: {
-            events: () => {
-                // return events;
-                // retorna toda a informação de uma relação
-                return Event.find()
-                    .then( events => {
-                        return events.map(event => {
-                            return { 
-                                ...event._doc, 
-                                _id: event.id,
-                                creator: user.bind(this, event._doc.creator)
-                            }
-                        });
-                    })
-                    .catch( err => {
-                        throw err;
-                    });
-            },
-            // CreateEvent e o CreateUser são os -> RESOLVERS
-            createEvent: (args) => {                
-                const event = new Event({
-                    title: args.eventInput.title,
-                    description: args.eventInput.description,
-                    price: +args.eventInput.price, // o símbolo + converte o que virá no args para um número
-                    date: new Date(args.eventInput.date),
-                    creator: '5dcc17d7bd92c7375cbe0c72'
-                });
-                let createdEvent;
-                return event
-                    .save()
-                    .then( result => {
-                        createdEvent = { 
-                            ...result._doc, 
-                            _id: result._doc_id,
-                            creator: user.bind(this, result._doc.creator) 
-                        };
-                        // hardcode para testes o ID é de user especifico
-                        return User.findById('5dcc17d7bd92c7375cbe0c72');
-                        // console.log(result);
-                        // return { ...result._doc, _id: result._doc_id.toString() };
-                    })
-                    .then( user => {
-                        if (!user) {
-                            throw new Error('User not found.');
-                        }
-                        user.createdEvents.push(event);
-                        return user.save();
-                    })
-                    .then(result => {
-                        return createdEvent;
-                    })
-                    .catch( err => {
-                        console.log(err);
-                        throw err;
-                    });
-                // PUSH event é para o graphql test no browser
-                //events.push(event);
-                return event;
-            },
-            createUser: (args) => {
-                // verificar se já existe algum user com o email
-                // a função findOne faz o filto pelo argumento email para comparar na BD dos users
-                return User
-                .findOne({email: args.userInput.email})
-                .then( user => {
-                    if (user) {
-                        throw new Error('User exists already.');
-                    }
-                    return bcrypt.hash(args.userInput.password, 12); // faz a encriptação da pass e o nível é o 12 (safe)
-                })
-                // retornar dados do utilizar (incluindo a pwd encriptada) quando criado o user para a BD 
-                .then( hashedPassword => {
-                    const user = new User({
-                        email: args.userInput.email,
-                        password: hashedPassword
-                    });
-                    return user.save();
-                })
-                .then( result => {
-                    return { ...result._doc, password: null,  _id: result._doc_id };
-                })
-                .catch( err => {
-                    throw err;
-                }); 
-            }
-
-        },
+        schema: graphQlSchema,
+        rootValue: graphQlResolvers,
         graphiql: true // opção para aparecer a ferramenta gráfica do GraphQL
 }));
 
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,120 @@
+const bcrypt = require('bcryptjs'); // Pack para fazer a encriptação das passwords
+
+// Importação do modelo do Event
+// ./ -> um nivel de subpasta 
+// ../ -> dois niveis de subpastas
+const Event = require('../../models/event');
+// Importação do modelo do User
+const User = require('../../models/user');
+
+// buscar(fetch) multiplos ID
+// eventIds é o argumento
+// com o async não é necessário return e substituir por await
+const events = async eventIds => {
+    // $in operador especial do MongoDB
+    try {
+        const events = await Event.find({ _id: { $in: eventIds } })
+                events.map(event => {
+                    return { 
+                        ...event._doc, 
+                        _id: event.id,
+                        date: new Date(event._doc.date).toISOString(), 
+                        creator: user.bind(this, event.creator) 
+                    };
+                });
+                return events;
+        } catch (err) {
+            throw err;
+        }
+}
+
+const user = async userId => {
+    try{
+    const user = await User.findById(userId)
+            return { 
+                ...user._doc, 
+                _id: user.id, 
+                createdEvents: events.bind(this, user._doc.createdEvents) 
+            };
+        } catch (err) {
+            throw err;
+        }
+};
+
+module.exports = {
+    events: async () => {
+        // return events;
+        // retorna toda a informação de uma relação
+        try{
+        const events = await Event.find()
+            return events.map(event => {
+                    return { 
+                        ...event._doc, 
+                        _id: event.id,
+                        date: new Date(event._doc.date).toISOString(),
+                        creator: user.bind(this, event._doc.creator)
+                    }
+            })
+        } catch (err) {
+                throw err;
+            };
+    },
+    // CreateEvent e o CreateUser são os -> RESOLVERS
+    createEvent: async args => {                
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price, // o símbolo + converte o que virá no args para um número
+            date: new Date(args.eventInput.date),
+            creator: '5dcc17d7bd92c7375cbe0c72'
+        });
+        let createdEvent;
+        try {
+        const result = await event
+            .save()
+                createdEvent = { 
+                    ...result._doc, 
+                    _id: result._doc_id,
+                    date: new Date(event._doc.date).toISOString(), 
+                    creator: user.bind(this, result._doc.creator) 
+                };
+                // hardcode para testes o ID é de user especifico
+                const user = await User.findById('5dcc17d7bd92c7375cbe0c72');
+                // console.log(result);
+                // return { ...result._doc, _id: result._doc_id.toString() };
+                if (!user) {
+                    throw new Error('User not found.');
+                }
+                user.createdEvents.push(event);
+                await user.save();
+                return createdEvent;
+        } catch (err) {    
+                console.log(err);
+                throw err;
+        }
+        // PUSH event é para o graphql test no browser
+        //events.push(event);
+        return event;
+    },
+    createUser: async args => {
+        // verificar se já existe algum user com o email
+        // a função findOne faz o filto pelo argumento email para comparar na BD dos users
+        try{
+        const existingUser = await User.findOne({email: args.userInput.email})
+            if (existingUser) {
+                throw new Error('User exists already.');
+            }
+            const hashedPassword = await bcrypt.hash(args.userInput.password, 12); // faz a encriptação da pass e o nível é o 12 (safe)
+        // retornar dados do utilizar (incluindo a pwd encriptada) quando criado o user para a BD    
+            const user = new User({
+                email: args.userInput.email,
+                password: hashedPassword
+            });
+            const result = await user.save();
+        
+            return { ...result._doc, password: null,  _id: result._doc_id };
+        } catch (err) {
+            throw err;
+        }
+    }
+}
