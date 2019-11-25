0d8324444424fd508cc863c839f240b157d310cc
Miguel Rocha <miguelrocha@lvengine.com>
2019-11-25 17:55:11 +0000

    Events List & Item

---
+++ frontend/src/components/Events/EventList/EventItem/EventItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './EventItem.css';
+
+const eventItem = props => (
+    <li key={props.eventId} className="events__list-item"> 
+        <div>
+             <h1>{props.title}</h1>
+             <h2>$19.9</h2>
+        </div>
+        <div>
+            {props.userId === props.creatorId ? <p>Your the owner this event.</p> : <button className="btn">View Details</button>}
+            
+        </div>
+    </li>
+);
+
+export default eventItem;
